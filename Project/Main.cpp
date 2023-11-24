9e03ad6fdf0aff2b2d80dc5218eda46def97f939
azoiteidaria <101119113+azoiteidaria@users.noreply.github.com>
2023-11-24 18:50:11 +0200

    Update Main.cpp

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,24 +1,103 @@
 // Main.cpp
 #include <iostream>
-#include <cstdlib> // For rand() function
 #include "Location.h"
-
-
+#include "Event.h"
 using namespace std;
 
+Location::Location() : maxSeats(0), numRows(0), numZones(0), seatsPerRow(0) {}
+
+Location::Location(int maxSeats, int numRows, int numZones, int seatsPerRow)
+    : maxSeats(maxSeats), numRows(numRows), numZones(numZones), seatsPerRow(seatsPerRow) {}
+
+int Location::getMaxSeats() const {
+    return maxSeats;
+}
+
+int Location::getNumRows() const {
+    return numRows;
+}
+
+int Location::getNumZones() const {
+    return numZones;
+}
+
+int Location::getSeatsPerRow() const {
+    return seatsPerRow;
+}
+
+void Location::setMaxSeats(int maxSeats) {
+
+    this->maxSeats = maxSeats;
+}
+
+void Location::setNumRows(int numRows) {
+
+    this->numRows = numRows;
+}
+
+void Location::setNumZones(int numZones) {
+
+    this->numZones = numZones;
+}
+
+void Location::setSeatsPerRow(int seatsPerRow) {
+    this->seatsPerRow = seatsPerRow;
+}
+
+Event::Event() {}
+
+Event::Event(const string& name, const string& date, const string& time)
+    : name(name), date(date), time(time) {}
+
+const string& Event::getName() const {
+    return name;
+}
+
+const string& Event::getDate() const {
+    return date;
+}
+
+const string& Event::getTime() const {
+    return time;
+}
+
+void Event::setName(const string& name) {
+    
+    this->name = name;
+}
+
+void Event::setDate(const string& date) {
+    
+    this->date = date;
+}
+
+void Event::setTime(const string& time) {
+   
+    this->time = time;
+}
+
 int main() {
-    // Create a Location
+  
     Location eventLocation;
-    eventLocation.setMaxSeats(1000);
-    eventLocation.setNumRows(20);
+    eventLocation.setMaxSeats(50000);
+    eventLocation.setNumRows(50);
     eventLocation.setNumZones(3);
-    eventLocation.setSeatsPerRow(50);
+    eventLocation.setSeatsPerRow(300);
 
+    
+    Event event("Concert Coldplay", "2024-06-13", "19:00");
 
-    // Display Location and Event details
+   
     cout << "Event Location Details:\n";
     cout << "Max Seats: " << eventLocation.getMaxSeats() << "\n";
     cout << "Number of Rows: " << eventLocation.getNumRows() << "\n";
     cout << "Number of Zones: " << eventLocation.getNumZones() << "\n";
     cout << "Seats per Row: " << eventLocation.getSeatsPerRow() << "\n\n";
+
+    cout << "Event Details:\n";
+    cout << "Name: " << event.getName() << "\n";
+    cout << "Date: " << event.getDate() << "\n";
+    cout << "Time: " << event.getTime() << "\n\n";
+
+    return 0;
 }
